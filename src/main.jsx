a9c9f6723068290fef061886fe68a2288a26d574
AkioMorii <orlandoakio@gmail.com>
2023-09-16 00:54:14 -0300

    router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Navbar from './components/navbar/NavbarComp'
+import './main.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
   </React.StrictMode>
 );
