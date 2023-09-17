24451ad413ddd6d5e141dd7b32d00c0a96dfb565
AkioMorii <orlandoakio@gmail.com>
2023-09-17 18:03:28 -0300

    projeto_SRC_adequacao

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-//teste
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Navbar from './components/navbar/NavbarComp'
+import './main.css'
+import Footer from './components/footer/footer'
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar/>
+    <Footer/>
   </React.StrictMode>
 );
